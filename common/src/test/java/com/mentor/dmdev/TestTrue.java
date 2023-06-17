a1a661a2403bff91ec70475fb462286c59bbd1eb
Мурзабек Суюндиков <murzabekmackbook@MacBook-Pro-Murzabek.local>
2023-06-17 16:51:28 +0300

    HOMEWORK-1: Сделать структуру проекта first-maven

---
+++ common/src/test/java/com/mentor/dmdev/TestTrue.java
@@ -0,0 +1,14 @@
+package com.mentor.dmdev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestTrue {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
---
+++ service/src/test/java/com/mentor/dmdev/integration/ExampleIT.java
@@ -0,0 +1,16 @@
+package com.mentor.dmdev.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleIT {
+
+	@Test
+	void test() {
+		assertTrue(true);
+		System.out.println("Hello from IntegrationTest");
+	}
+
+
+}
